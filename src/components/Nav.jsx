7e8ebc1643c7b25c488bd0ba66eafd9cfdee5bc6
Mauricio Romero <mauricionromero@hotmail.com>
2021-10-08 11:30:56 -0300

    updated link to Works

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { StyledNav, StyledNavLink } from "./StyledComponents";
 export default function Nav({ showContact }) {
   return (
     <StyledNav>
-      <StyledNavLink href="http:#work">Works</StyledNavLink>
+      <StyledNavLink href="#work">Works</StyledNavLink>
       <StyledNavLink onClick={() => showContact()}>Contact</StyledNavLink>
     </StyledNav>
   );
